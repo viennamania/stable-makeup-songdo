f37d07e96ad222f9bbdab9ffefe7c67d8a8c4361
seunghyonpark <creath.park@gmail.com>
2025-10-16 22:38:10 +0900

    feat: implement sendCKEC API endpoint for transferring CKEC tokens to user wallets

---
+++ src/app/api/user/sendCKEC/route.ts
@@ -0,0 +1,203 @@
+import { NextResponse, type NextRequest } from "next/server";
+
+import {
+	getOneByNickname,
+} from '@lib/api/user';
+
+import {
+  createThirdwebClient,
+  getContract,
+  sendTransaction,
+} from "thirdweb";
+
+
+
+import {
+  balanceOf,
+  transfer,
+} from "thirdweb/extensions/erc20";
+ 
+import {
+  ethereum,
+  polygon,
+  arbitrum,
+  bsc,
+} from "thirdweb/chains";
+
+import {
+  chain,
+  ethereumContractAddressUSDT,
+  polygonContractAddressUSDT,
+  arbitrumContractAddressUSDT,
+  bscContractAddressUSDT,
+
+  arbitrumContractAddressCKEC,
+} from "@/app/config/contractAddresses";
+
+
+import {
+  privateKeyToAccount,
+  smartWallet,
+ } from "thirdweb/wallets";
+
+
+
+
+export async function POST(request: NextRequest) {
+
+  const body = await request.json();
+
+  //console.log("body", body);
+
+  const {
+    storecode,
+    userCode,
+    toWalletAddress,
+    amount,
+  } = body;
+
+  if (!amount || isNaN(amount) || amount <= 0) {
+    return NextResponse.json({
+      error: "Invalid amount",
+    }, { status: 400 });
+  }
+
+  if (amount > 1000) {
+    return NextResponse.json({
+      error: "Amount exceeds the maximum limit of 1000 CKEC",
+    }, { status: 400 });
+  }
+
+
+
+  const result = await getOneByNickname(
+    storecode,
+    userCode,
+  );
+
+  if (!result) {
+    return NextResponse.json({
+      error: "User not found",
+    }, { status: 404 });
+  }
+
+  const walletAddress = result.walletAddress;
+  const walletPrivateKey = result.walletPrivateKey;
+
+
+
+  const client = createThirdwebClient({
+    secretKey: process.env.THIRDWEB_SECRET_KEY || "",
+  });
+
+  const contract = getContract({
+    // the client you have created via `createThirdwebClient()`
+    client,
+    // the chain the contract is deployed on
+    
+    
+    //chain: arbitrum,
+    chain:  chain === "ethereum" ? ethereum :
+            chain === "polygon" ? polygon :
+            chain === "arbitrum" ? arbitrum :
+            chain === "bsc" ? bsc : arbitrum,
+  
+  
+  
+    // the contract's address
+    ///address: contractAddressArbitrum,
+
+    address: chain === "ethereum" ? arbitrumContractAddressCKEC :
+            chain === "polygon" ? arbitrumContractAddressCKEC :
+            chain === "arbitrum" ? arbitrumContractAddressCKEC :
+            chain === "bsc" ? arbitrumContractAddressCKEC : arbitrumContractAddressCKEC,
+
+
+    // OPTIONAL: the contract's abi
+    //abi: [...],
+  });
+
+
+  /*
+  const resultBalance = await balanceOf({
+    contract,
+    address: walletAddress,
+  });
+
+
+  const balance = Number(resultBalance) / 10 ** 18;
+  */
+
+
+
+  // transfer from mint wallet to user's wallet
+  try {
+
+    const personalAccount = privateKeyToAccount({
+      client,
+      privateKey: walletPrivateKey,
+    });
+
+    if (!personalAccount) {
+      return NextResponse.json({
+        error: "Mint wallet account not found",
+      }, { status: 500 });
+    }
+
+    const wallet = smartWallet({
+      chain: chain === "ethereum" ? ethereum :
+             chain === "polygon" ? polygon :
+             chain === "arbitrum" ? arbitrum :
+             chain === "bsc" ? bsc : arbitrum,
+
+      //factoryAddress: "0x655934C0B4bD79f52A2f7e6E60714175D5dd319b", // your own deployed account factory address
+      sponsorGas: true,
+    });
+
+    // Connect the smart wallet
+    const account = await wallet.connect({
+      client: client,
+      personalAccount: personalAccount,
+    });
+
+    if (!account) {
+      return NextResponse.json({
+        result: null,
+      });
+    }
+
+
+
+
+    const transaction = transfer({
+      contract,
+      to: toWalletAddress,
+      amount: amount,
+    });
+
+    const { transactionHash } = await sendTransaction({
+      account,
+      transaction,
+    });
+
+
+    return NextResponse.json({
+
+      result: {
+        nickname: userCode,
+        amount: amount,
+        toWalletAddress: toWalletAddress,
+        transactionHash: transactionHash,
+        embeddedWalletAddress: walletAddress,
+      }
+    
+    });
+
+  } catch (error) {
+    console.error("Minting error:", error);
+    return NextResponse.json({
+      error: "Minting failed",
+    }, { status: 500 });
+  }
+  
+}
